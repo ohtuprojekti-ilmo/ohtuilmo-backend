17b4f88a6322f67919f261c748f2e7df30aae79e
ankatus <antti.j.kataja@gmail.com>
2018-09-27 21:41:11 +0300

    succesful login now returns fields in user object

--- controllers/login.js
+++ controllers/login.js
@@ -7,7 +7,7 @@ module.exports = function (passport, path) {
         if (req.isAuthenticated()) {
             console.log('success')
             console.log(req.user.username)
-            res.status(200).send(req.user)
+            res.status(200).send( {user: req.user})
         } else {
             res.redirect(`${path}/failTest`)
         }
