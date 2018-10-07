7d39bc4a6e4ebad456e67cae1c5d5fa422994873
vasdf <vasdf@users.noreply.github.com>
2018-10-07 20:52:47 +0300

    Update user.js
    student_number changed from integer to string

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define('user', {
     student_number: {
-      type: Sequelize.INTEGER,
+      type: Sequelize.STRING,
       primaryKey: true
     },
     username: {
