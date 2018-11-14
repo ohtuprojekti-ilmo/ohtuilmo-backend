a5523a8d482ff0d65af7db564a927647f2faba57
ankatus <antti.j.kataja@gmail.com>
2018-11-14 22:41:13 +0200

    changed get to check against normal and secret id

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,11 +1,16 @@
 const topicsRouter = require('express').Router()
 const db = require('../models/index')
 const checkAdmin = require('../utils/middleware/checkAdmin').checkAdmin
+const getRandomId = require('../utils/idGeneration').getRandomId
+const Op = db.Sequelize.Op
 
 topicsRouter.post('/', (req, res) => {
   if (!req.body.content) return res.status(400).json({ error: 'content undefined' })
+  const secret_id = getRandomId()
   db.Topic.create({
-    content: req.body.content
+    content: req.body.content,
+    acronym: req.body.acronym,
+    secret_id
   })
     .then(topic => {
       res.status(200).json({ topic })
@@ -22,7 +27,8 @@ topicsRouter.put('/:id', checkAdmin, (req, res) => {
       if (!topic) return res.status(400).json({ error: 'no topic with that id' })
       topic.update({
         active: req.body.active,
-        content: req.body.content
+        content: req.body.content,
+        acronym: req.body.acronym
       })
         .then(topic => {
           topic.reload().then(topic => {
@@ -52,7 +58,14 @@ topicsRouter.get('/', (req, res) => {
 })
 
 topicsRouter.get('/:id', (req, res) => {
-  db.Topic.findOne({ where: { topic_id: req.params.id } })
+  db.Topic.findOne({
+    where: {
+      [Op.or]: [
+        { topic_id: req.params.id },
+        { secret_id: req.params.id }
+      ]
+    }
+  })
     .then(topic => {
       res.status(200).json(topic)
     })
