95894ffd91fc07660ccfb65210c809c910c4f648
ankatus <antti.j.kataja@gmail.com>
2018-11-22 15:13:09 +0200

    removed unused stuff

--- utils/config.js
+++ utils/config.js
@@ -5,11 +5,6 @@ if (process.env.NODE_ENV !== 'production') {
 let port = process.env.PORT
 let dbUrl = process.env.DATABASE_URI
 
-if (process.env.NODE_ENV === 'test') {
-  port = process.env.TEST_PORT
-  dbUrl = process.env.TEST_DATABASE_URI
-}
-
 let login = 'http://opetushallinto.cs.helsinki.fi/login'
 
 let secret = 'secret'
