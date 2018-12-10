18a137e859018f6265200cc402c256c17622cbb1
ankatus <antti.j.kataja@gmail.com>
2018-12-10 22:49:41 +0200

    changed default email config a bit

--- utils/config.js
+++ utils/config.js
@@ -9,14 +9,32 @@ let login = 'http://opetushallinto.cs.helsinki.fi/login'
 
 let secret = process.env.SECRET
 
-//email config
+// default email config
 let email = {
-  sender: 'Ohtuilmo Robot <noreply@helsinki.fi>',
-  host: 'smtp.helsinki.fi',
-  port: 587,
-  secure: false,
-  replyTo: 'mluukkai@cs.helsinki.fi',
-  cc: 'mluukkai@cs.helsinki.fi'
+  general: {
+    sender: 'Ohtuilmo Robot <noreply@helsinki.fi>',
+    host: 'smtp.helsinki.fi',
+    port: 587,
+    secure: false,
+    replyTo: 'mluukkai@cs.helsinki.fi',
+    cc: 'mluukkai@cs.helsinki.fi'
+  },
+  acceptEng: {
+    subject: '',
+    html: ''
+  },
+  rejectEng: {
+    subject: '',
+    html: ''
+  },
+  acceptFin: {
+    subject: '',
+    html: ''
+  },
+  rejectFin: {
+    subject: '',
+    html: ''
+  }
 }
 
 module.exports = {
