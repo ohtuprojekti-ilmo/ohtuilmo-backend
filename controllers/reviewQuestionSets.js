6d962d2e9e72d4697f4d427e5163cf1084602a5c
ankatus <antti.j.kataja@gmail.com>
2018-12-13 14:36:13 +0200

    added checks and fixed bugs

--- controllers/reviewQuestionSets.js
+++ controllers/reviewQuestionSets.js
@@ -13,7 +13,7 @@ const createReviewQuestionSet = (req, res) => {
       name: req.body.name,
       questions: req.body.questions
     })
-    .then(createdSet => res.status(200).json({ createdSet }))
+    .then(createdSet => res.status(200).json({ questionSet: createdSet }))
     .catch(error => handleDatabaseError(res, error))
 }
 
@@ -27,31 +27,53 @@ const updateReviewQuestionSet = (req, res, questionSet) => {
       questionSet
         .reload()
         .then(updatedSet => {
-          res.status(200).json({ updatedSet })
+          res.status(200).json({ questionSet: updatedSet })
         })
         .catch(error => handleDatabaseError(res, error))
     })
     .catch(error => handleDatabaseError(res, error))
 }
 
+const createChecks = (req, res) => {
+  if (!req.body.name) return res.status(400).json({ error: 'name undefined' })
+  db.ReviewQuestionSet
+    .findOne({ where: { name: req.body.name } })
+    .then(foundSet => {
+      if (foundSet) return res.status(400).json({ error: 'name already in use' })
+      createReviewQuestionSet(req, res)
+    })
+    .catch(error => handleDatabaseError(res, error))
+}
+
+const updateChecks = (req, res) => {
+  if (isNaN(req.params.id)) return res.status(400).json({ error: 'invalid id' })
+  if (!req.body.id) return res.status(400).json({ error: 'name undefined' })
+  db.ReviewQuestionSet
+    .findOne({ where: { name: req.body.name } })
+    .then(duplicateNameSet => {
+      if (duplicateNameSet) return res.status(400).json({ error: 'name already in use' })
+      db.ReviewQuestionSet
+        .findOne({ where: { id: req.params.id } })
+        .then(foundSet => {
+          if (!foundSet) return res.status(400).json({ error: 'no review question set with that id' })
+          updateReviewQuestionSet(req, res, foundSet)
+        })
+    })
+}
+
 reviewQuestionSetsRouter.post('/', checkAdmin, (req, res) => {
-  createReviewQuestionSet(req, res)
+  createChecks(req, res)
 })
 
 reviewQuestionSetsRouter.put('/:id', checkAdmin, (req, res) => {
-  db.ReviewQuestionSet
-    .findOne({ where: { id: req.params.id } })
-    .then(foundSet => {
-      if (!foundSet) return res.status(400).json({ error: 'no registration_question with that id' })
-      updateReviewQuestionSet(req, res, foundSet)
-    })
+  updateChecks(req, res)
 })
 
 reviewQuestionSetsRouter.get('/', checkAdmin, (req, res) => {
   db.ReviewQuestionSet
     .findAll({})
-    .then(questionSets => {
-      res.status(200).json({ questionSets })
+    .then(foundSets => {
+      res.status(200).json({ questionSets: foundSets })
     })
     .catch(error => handleDatabaseError(res, error))
 })
