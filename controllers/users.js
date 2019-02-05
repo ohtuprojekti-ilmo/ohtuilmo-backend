aa8f1c5a0fe7d103abfca5d949e9c92ba0ef82fc
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-02-05 17:56:20 +0200

    Tidy users controller with async/await

--- controllers/users.js
+++ controllers/users.js
@@ -2,44 +2,33 @@ const usersRouter = require('express').Router()
 const db = require('../models/index')
 const { checkLogin } = require('../middleware')
 
-const checkValidStudentNumber = (req, res, next) => {
-  if (req.user.id !== req.params.student_number) {
+usersRouter.put('/:studentNumber', checkLogin, async (req, res) => {
+  const { email } = req.body
+  const { studentNumber } = req.params
+
+  if (req.user.id !== studentNumber) {
     return res.status(401).json({ error: 'student numbers not matching' })
   }
 
-  next()
-}
-
-usersRouter.put(
-  '/:student_number',
-  checkLogin,
-  checkValidStudentNumber,
-  (req, res) => {
-    if (!req.body.email) return res.status(400).json({ error: 'missing email' })
-
-    db.User.findOne({ where: { student_number: req.params.student_number } })
-      .then((user) => {
-        if (!user) return res.status(400).json({ error: 'user does not exist' })
-
-        user
-          .update({
-            email: req.body.email
-          })
-          .then((user) => {
-            user.reload().then((user) => {
-              res.status(200).json({ user })
-            })
-          })
-          .catch((error) => {
-            console.log(error)
-            res.status(500).json({ error: 'database error' })
-          })
-      })
-      .catch((error) => {
-        console.log(error)
-        res.status(500).json({ error: 'database error' })
-      })
+  if (!email) {
+    return res.status(400).json({ error: 'missing email' })
   }
-)
+
+  try {
+    const user = await db.User.findOne({
+      where: { student_number: studentNumber }
+    })
+    if (!user) {
+      return res.status(400).json({ error: 'user does not exist' })
+    }
+
+    const updatedUser = await user.update({ email })
+    const refreshedUser = await updatedUser.reload()
+    res.status(200).json({ refreshedUser })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'database error' })
+  }
+})
 
 module.exports = usersRouter
