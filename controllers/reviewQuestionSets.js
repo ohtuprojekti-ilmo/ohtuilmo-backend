1c1899aec5e16a6e9bde4ce9b820958c138628fa
Miika Hyttinen <hyttinenmiika@gmail.com>
2019-03-08 13:39:15 +0200

    removed debug console log

--- controllers/reviewQuestionSets.js
+++ controllers/reviewQuestionSets.js
@@ -91,7 +91,6 @@ reviewQuestionSetsRouter.get('/:id', checkLogin, async (req, res) => {
 
 reviewQuestionSetsRouter.delete('/:id', checkAdmin, async (req, res) => {
   const questionSetId = parseInt(req.params.id, 10)
-  console.log('******ID********', req.params.id)
   if (isNaN(questionSetId)) {
     return res.status(400).json({ error: 'invalid id' })
   }
